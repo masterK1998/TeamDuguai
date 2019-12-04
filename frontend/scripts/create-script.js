15f12f64f404e19d935d8328c111411af553849f
unknown <liruochenv@gmail.com>
2019-12-03 20:25:04 -0500

    fix

--- frontend/scripts/create-script.js
+++ frontend/scripts/create-script.js
@@ -1,29 +1,16 @@
 $(document).ready(function () {
     $('.main').on('click', '#create', async function(){      
-<<<<<<< HEAD
         let username = $('#username').val();
         let passWord = $('#password').val();
-=======
-        let emailAddress = $('#Username').val();
-        let password = $('#Password').val();
-        let onyen = $('#Onyen').val();
-        let pid = $('#PID').val();
->>>>>>> 7bf74f67f8f6dc700c7b0789dce2d70c76a8949b
         let result  = await axios({
             method: 'POST',
             url: 'http://localhost:3000/account/create',
             data: {
-<<<<<<< HEAD
                     "name": username,
                     "pass": passWord,
-=======
-                    "name": emailAddress,
-                    "password": password,
-                    "onyen": onyen,
-                    "pid": pid
->>>>>>> 7bf74f67f8f6dc700c7b0789dce2d70c76a8949b
             }
         });  
         window.location.replace("../login/index.html");
     });
 });
+    
--- frontend/scripts/login-script.js
+++ frontend/scripts/login-script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-<<<<<<< HEAD
     $('.main').on('click', '#login', async function(){      
         let username = $('#username').val();
         let passWord = $('#password').val();
@@ -15,25 +14,5 @@ $(document).ready(function () {
         
         localStorage.setItem("jwt", result.data.jwt);
         //window.location.replace("../index.html");
-=======
-    $('.main').on('click', '#signIn', async function(){      
-        let emailAddress = $('#loginUser').val();
-        let password = $('#loginPass').val();
-        let match = false;
-        let result  = await axios({
-            method: 'GET',
-            url: 'http://localhost:3000/account/login',
-            withCredentials: true,
-        });
-        
-        for(let i  = 0; i < result.data.length; i++) {
-            if(result.data[i].name == emailAddress) {
-                match = true;
-                if(result.data[i].password == password) console.log("success!");
-                else console.log("wrong password");
-            }
-        }
-        if(!match) console.log("user not found");
->>>>>>> 7bf74f67f8f6dc700c7b0789dce2d70c76a8949b
     });
 });
